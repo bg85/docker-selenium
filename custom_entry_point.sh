0ac01bedac0c09e87aa393b1f6e49fd4ae6f12f6
Barbara Gonzalez <bgonzalez@ipcoop.com>
2016-08-18 16:54:04 -0400

    adding chrome

--- custom_entry_point.sh
+++ custom_entry_point.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "running my custom image"
+ifconfig -a
 
+ADPT_PARAM="eth0"
+if [ ! -z "$HOST_ADAPTER" ]; then
+  echo "HOST_ADAPTER variable is set, using adapter IP"
+  ADPT_PARAM="$HOST_ADAPTER"
+fi
 
-sh /opt/bin/entry_point.sh
+REMOTE_HOST=http://`ifconfig $ADPT_PARAM | grep 'inet addr' | cut -d: -f2 | awk '{print $1}'`:5555
+
+export REMOTE_HOST
+echo "REMOTE_HOST: $REMOTE_HOST"
+
+./opt/bin/entry_point.sh
